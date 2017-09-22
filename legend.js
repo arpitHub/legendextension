2cc39d1750806400e5939443e09059778a539d44
Arpit Rawat <rawat.a@husky.neu.edu>
2017-09-22 16:23:06 -0400

    added color picker

--- legend.js
+++ legend.js
@@ -1,16 +1,17 @@
 define(
     [
         'jquery',
-        "css!./legend.css"
+        "css!./legend.css",
+		"text!themes/old/sense/theme.json"
     ],
-    function($, cssContent) {
+    function($, cssContent, Theme) {
         'use strict';
+		var Theme = JSON.parse(Theme);
         return {
             definition: {
                 type: "items",
                 component: "accordion",
                 items: {
-
                     appearance: {
                         uses: "settings",
                         items: {
@@ -19,7 +20,7 @@ define(
                                 label: "First Legend",
                                 items: {
                                     visibleBox: {
-                                        label: "Is Visible?",
+                                        label: "Is It Visible?",
                                         ref: "myVisibleBox1",
                                         type: "boolean",
                                         component: "switch",
@@ -33,6 +34,13 @@ define(
                                             }
                                         ]
                                     },
+									MyColorPicker: {
+							          label:"My color-picker",
+							          component: "color-picker",
+							          ref: "myColor1",
+							          type: "integer",
+							          defaultValue: 3
+						            },
                                     textBox: {
                                         label: "Name",
                                         ref: "myTextBox1",
@@ -82,6 +90,13 @@ define(
                                             }
                                         ]
                                     },
+									MyColorPicker: {
+							          label:"My color-picker",
+							          component: "color-picker",
+							          ref: "myColor2",
+							          type: "integer",
+							          defaultValue: 3
+						            },
                                     textBox: {
                                         label: "Name",
                                         ref: "myTextBox2",
@@ -142,6 +157,13 @@ define(
                                         type: "string",
                                         expression: "optional"
                                     },
+									MyColorPicker: {
+							          label:"My color-picker",
+							          component: "color-picker",
+							          ref: "myColor3",
+							          type: "integer",
+							          defaultValue: 3
+						            },
                                     typeBox: {
                                         label: "Line/Box",
                                         ref: "myTypeBox3",
@@ -190,6 +212,13 @@ define(
                                         type: "string",
                                         expression: "optional"
                                     },
+									MyColorPicker: {
+							          label:"My color-picker",
+							          component: "color-picker",
+							          ref: "myColor4",
+							          type: "integer",
+							          defaultValue: 1
+						            },
                                     typeBox: {
                                         label: "Line/Box",
                                         ref: "myTypeBox4",
@@ -219,24 +248,25 @@ define(
             },
 
             paint: function($element, layout) {
-
-
                 $element.empty();
+				console.log(Theme);
                 var legendSpecs = {
                     measureName: [layout.myTextBox1, layout.myTextBox2, layout.myTextBox3, layout.myTextBox4],
-                    background_color: [layout.myColorBox1, layout.myColorBox2, layout.myColorBox3, layout.myColorBox4],
+                    background_color: [layout.myColor1, layout.myColor2, layout.myColor3, layout.myColor4],
                     type: [layout.myTypeBox1, layout.myTypeBox2, layout.myTypeBox3, layout.myTypeBox4],
                     visible: [layout.myVisibleBox1, layout.myVisibleBox2, layout.myVisibleBox3, layout.myVisibleBox4]
 
                 }
                 //console.log(document.getElementById(layout.qInfo.qId), layout.qHyperCube ) ;
+				//console.log(mediaTool.palette[layout.myColor]);
+				//$element.css("background-color", palette[layout.myColor]);
                 var legend = '\
 					<div class="legend" id="' + layout.qInfo.qId + '">';
                 var columnWidth = ($element.width()/4 )< 140 ? 140  : ($element.width()/4) ;
                 for (var i = 0; i < legendSpecs.measureName.length; i++) {
                     if (legendSpecs.visible[i]) {
                         let legendType = legendSpecs.type[i] ? 'box' : 'line';
-                        legend += '<div class="column" style="font-size:12px' + ';width:' + columnWidth + 'px' + '"><div class="' + legendType + '" style="background-color:' + legendSpecs.background_color[i] + '"></div>' + '<b>' + legendSpecs.measureName[i] + '</b>' + '</div>';
+                        legend += '<div class="column" style="font-size:12px' + ';width:' + columnWidth + 'px' + '"><div class="' + legendType + '" style="background-color:' + Theme.palette[legendSpecs.background_color[i]] + '"></div>' + '<b>' + legendSpecs.measureName[i] + '</b>' + '</div>';
                     }
                 }
 
