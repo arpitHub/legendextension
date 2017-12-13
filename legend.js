2e6fb7906e26cb4496218139ec0319f5a9ae9001
Arpit Rawat <rawat.a@husky.neu.edu>
2017-12-12 19:49:15 -0500

    Fixed Color-picker for September 2017 Qlik Sense release

--- legend.js
+++ legend.js
@@ -1,12 +1,9 @@
 define(
     [
-        'jquery',
-        "css!./legend.css",
-		"text!themes/old/sense/theme.json"
+        "jquery",
+        "css!./legend.css"
     ],
     function($, cssContent, Theme) {
-        'use strict';
-		var Theme = JSON.parse(Theme);
         return {
             definition: {
                 type: "items",
@@ -38,8 +35,11 @@ define(
 							          label:"My color-picker",
 							          component: "color-picker",
 							          ref: "myColor1",
-							          type: "integer",
-							          defaultValue: 3
+							          type: "object",
+							          defaultValue: {  
+                                        index: 3,  
+                                        color: "#4477aa"  
+                                      }
 						            },
                                     textBox: {
                                         label: "Name",
@@ -70,13 +70,12 @@ define(
                                     }
                                 }
                             },
-                            myTextBox2: {
+							myTextBox2: {
                                 type: "items",
                                 label: "Second Legend",
                                 items: {
-
                                     visibleBox: {
-                                        label: "Is Visible?",
+                                        label: "Is It Visible?",
                                         ref: "myVisibleBox2",
                                         type: "boolean",
                                         component: "switch",
@@ -94,8 +93,11 @@ define(
 							          label:"My color-picker",
 							          component: "color-picker",
 							          ref: "myColor2",
-							          type: "integer",
-							          defaultValue: 3
+							          type: "object",
+							          defaultValue: {  
+                                        index: 3,  
+                                        color: "#4477aa"  
+                                      }
 						            },
                                     textBox: {
                                         label: "Name",
@@ -125,17 +127,18 @@ define(
                                         ]
                                     }
                                 }
-                            },
-                            myTextBox3: {
+                            }
+							,
+							myTextBox3: {
                                 type: "items",
                                 label: "Third Legend",
                                 items: {
                                     visibleBox: {
-                                        label: "Is Visible?",
+                                        label: "Is It Visible?",
                                         ref: "myVisibleBox3",
                                         type: "boolean",
                                         component: "switch",
-                                        defaultValue: false,
+                                        defaultValue: true,
                                         options: [{
                                                 value: true,
                                             },
@@ -145,6 +148,16 @@ define(
                                             }
                                         ]
                                     },
+									MyColorPicker: {
+							          label:"My color-picker",
+							          component: "color-picker",
+							          ref: "myColor3",
+							          type: "object",
+							          defaultValue: {  
+                                        index: 3,  
+                                        color: "#4477aa"  
+                                      }
+						            },
                                     textBox: {
                                         label: "Name",
                                         ref: "myTextBox3",
@@ -157,13 +170,6 @@ define(
                                         type: "string",
                                         expression: "optional"
                                     },
-									MyColorPicker: {
-							          label:"My color-picker",
-							          component: "color-picker",
-							          ref: "myColor3",
-							          type: "integer",
-							          defaultValue: 3
-						            },
                                     typeBox: {
                                         label: "Line/Box",
                                         ref: "myTypeBox3",
@@ -181,16 +187,16 @@ define(
                                     }
                                 }
                             },
-                            myTextBox4: {
+							myTextBox4: {
                                 type: "items",
                                 label: "Fourth Legend",
                                 items: {
                                     visibleBox: {
-                                        label: "Is Visible?",
+                                        label: "Is It Visible?",
                                         ref: "myVisibleBox4",
                                         type: "boolean",
                                         component: "switch",
-                                        defaultValue: false,
+                                        defaultValue: true,
                                         options: [{
                                                 value: true,
                                             },
@@ -200,6 +206,16 @@ define(
                                             }
                                         ]
                                     },
+									MyColorPicker: {
+							          label:"My color-picker",
+							          component: "color-picker",
+							          ref: "myColor4",
+							          type: "object",
+							          defaultValue: {  
+                                        index: 3,  
+                                        color: "#4477aa"  
+                                      }
+						            },
                                     textBox: {
                                         label: "Name",
                                         ref: "myTextBox4",
@@ -212,16 +228,67 @@ define(
                                         type: "string",
                                         expression: "optional"
                                     },
-									MyColorPicker: {
-							          label:"My color-picker",
-							          component: "color-picker",
-							          ref: "myColor4",
-							          type: "integer",
-							          defaultValue: 1
-						            },
                                     typeBox: {
                                         label: "Line/Box",
                                         ref: "myTypeBox4",
+                                        type: "boolean",
+                                        component: "switch",
+                                        defaultValue: false,
+                                        options: [{
+                                                value: true,
+                                            },
+                                            {
+                                                value: false
+
+                                            }
+                                        ]
+                                    }
+                                }
+                            },
+							myTextBox5: {
+                                type: "items",
+                                label: "Fifth Legend",
+                                items: {
+                                    visibleBox: {
+                                        label: "Is It Visible?",
+                                        ref: "myVisibleBox5",
+                                        type: "boolean",
+                                        component: "switch",
+                                        defaultValue: true,
+                                        options: [{
+                                                value: true,
+                                            },
+                                            {
+                                                value: false
+
+                                            }
+                                        ]
+                                    },
+									MyColorPicker: {
+							          label:"My color-picker",
+							          component: "color-picker",
+							          ref: "myColor5",
+							          type: "object",
+							          defaultValue: {  
+                                        index: 2,  
+                                        color: "#4477aa"  
+                                      }
+						            },
+                                    textBox: {
+                                        label: "Name",
+                                        ref: "myTextBox5",
+                                        type: "string",
+                                        expression: "optional"
+                                    },
+                                    colorBox: {
+                                        label: "Color",
+                                        ref: "myColorBox5",
+                                        type: "string",
+                                        expression: "optional"
+                                    },
+                                    typeBox: {
+                                        label: "Line/Box",
+                                        ref: "myTypeBox5",
                                         type: "boolean",
                                         component: "switch",
                                         defaultValue: false,
@@ -249,31 +316,28 @@ define(
 
             paint: function($element, layout) {
                 $element.empty();
-				console.log(Theme);
                 var legendSpecs = {
-                    measureName: [layout.myTextBox1, layout.myTextBox2, layout.myTextBox3, layout.myTextBox4],
-                    background_color: [layout.myColor1, layout.myColor2, layout.myColor3, layout.myColor4],
-                    type: [layout.myTypeBox1, layout.myTypeBox2, layout.myTypeBox3, layout.myTypeBox4],
-                    visible: [layout.myVisibleBox1, layout.myVisibleBox2, layout.myVisibleBox3, layout.myVisibleBox4]
+                    measureName: [layout.myTextBox1, layout.myTextBox2, layout.myTextBox3, layout.myTextBox4,layout.myTextBox5],
+                    background_color: [layout.myColorBox1, layout.myColorBox2, layout.myColorBox3, layout.myColorBox4,layout.myColorBox5],
+					pallet_color: [layout.myColor1, layout.myColor2, layout.myColor3, layout.myColor4, layout.myColor5],
+                    type: [layout.myTypeBox1, layout.myTypeBox2, layout.myTypeBox3, layout.myTypeBox4,layout.myTypeBox4,layout.myTypeBox5],
+                    visible: [layout.myVisibleBox1, layout.myVisibleBox2, layout.myVisibleBox3, layout.myVisibleBox4, layout.myVisibleBox5]
 
                 }
-                //console.log(document.getElementById(layout.qInfo.qId), layout.qHyperCube ) ;
-				//console.log(mediaTool.palette[layout.myColor]);
 				//$element.css("background-color", palette[layout.myColor]);
                 var legend = '\
 					<div class="legend" id="' + layout.qInfo.qId + '">';
-                var columnWidth = ($element.width()/4 )< 140 ? 140  : ($element.width()/4) ;
+                var columnWidth = ($element.width()/5 )< 140 ? 140  : ($element.width()/5) ;
                 for (var i = 0; i < legendSpecs.measureName.length; i++) {
-                    if (legendSpecs.visible[i]) {
-                        let legendType = legendSpecs.type[i] ? 'box' : 'line';
-                        legend += '<div class="column" style="font-size:12px' + ';width:' + columnWidth + 'px' + '"><div class="' + legendType + '" style="background-color:' + Theme.palette[legendSpecs.background_color[i]] + '"></div>' + '<b>' + legendSpecs.measureName[i] + '</b>' + '</div>';
+                    if (legendSpecs.visible[i] && (legendSpecs.measureName[i] != '')) {
+					    let vColor = legendSpecs.background_color[i] == '' ? legendSpecs.pallet_color[i].color : legendSpecs.background_color[i];
+                        let legendType = legendSpecs.type[i] ? 'line' : 'box';
+                        legend += '<div class="column" style="font-size:12px' + ';width:' + columnWidth + 'px' + '"><div class="' + legendType + '" style="background-color:' + vColor + '"></div>' + '<b>' + legendSpecs.measureName[i] + '</b>' + '</div>';
                     }
                 }
 
                 legend += '</div>';
-
                 $element.html(legend);
-
             }
         };
     }
